3143dbbdcfacffe013268506660f0825c2f93b9f
pushpithagit <pushpithagit@gmail.com>
2025-10-15 20:08:06 +0530

    Add files via upload

---
+++ week1_day1.js
@@ -0,0 +1,10 @@
+let name = "Pushpitha"
+console.log(name + ": " + typeof(name))
+let comname="TESTLEAF"
+console.log(comname +": "+ typeof(comname))
+let mobnumber=908785647n
+console.log(mobnumber +": "+ typeof(mobnumber))
+let isAutomation = true;
+console.log(isAutomation +" :"+ typeof(isAutomation))
+let hasPlaywright
+console.log(typeof(hasPlaywright))
