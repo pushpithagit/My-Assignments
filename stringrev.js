6a185a5e23b635360d9e7aa0029dcb9207f3e31e
pushpithagit <pushpithagit@gmail.com>
2025-10-26 12:15:27 +0530

    Add files via upload

---
+++ stringrev.js
@@ -0,0 +1,16 @@
+let name="level"
+console.log("The original string is" +" "+ name)
+let rev=" "
+for(let i=name.length-1;i>=0;i--){
+    
+        
+        rev=rev+name.charAt(i)
+}
+if (rev==name) {
+    
+    
+}
+
+console.log("The reversed string is" + rev)
+        
+    
